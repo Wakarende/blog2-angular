c39732381743b9749ad3ec0bc42242e67031a61a
Joy Kirii <joykirii@gmail.com>
2021-06-09 18:29:22 +0300

    create blog service

--- blog/src/app/services/blogs/blog.service.ts
+++ blog/src/app/services/blogs/blog.service.ts
@@ -1,11 +1,43 @@
+import { Blog } from './../../blog';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+const baseUrl ='http://127.0.0.1:8000/api/blogs/'
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getAll(): Observable<Blog[]> {
+    return this.http.get<Blog[]>(baseUrl);
+  }
+
+  get(id: any): Observable<Blog> {
+    return this.http.get(`${baseUrl}/${id}`);
+  }
+
+  create(data: any): Observable<any> {
+    return this.http.post(baseUrl, data);
+  }
+
+  update(id: any, data: any): Observable<any> {
+    return this.http.put(`${baseUrl}/${id}`, data);
+  }
+
+  delete(id: any): Observable<any> {
+    return this.http.delete(`${baseUrl}/${id}`);
+  }
+
+  deleteAll(): Observable<any> {
+    return this.http.delete(baseUrl);
+  }
+
+  findByTitle(title: any): Observable<Blog[]> {
+    return this.http.get<Blog[]>(`${baseUrl}?title=${title}`);
+  }
+
 }
