15620102edb2de69de5935a8eea220afef3d5776
Joy Kirii <joykirii@gmail.com>
2021-06-09 23:56:42 +0300

    create register function

--- blog/src/app/components/auth/register/register.component.ts
+++ blog/src/app/components/auth/register/register.component.ts
@@ -1,4 +1,8 @@
+import { ActivatedRoute,Router } from '@angular/router';
+import { FormGroup,FormControl } from '@angular/forms';
+import { RegisterService } from './../../../services/auth/register.service';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registrationform: FormGroup;
+
+  constructor(
+    private registerservice:RegisterService,
+    private route: ActivatedRoute, 
+    private router: Router
+    )
+     { }
 
   ngOnInit(): void {
+    this.registrationform= new FormGroup({
+      username: new FormControl(''),
+      email: new FormControl(''),
+      password: new FormControl(''),
+    });
   }
+
+  get registration(){
+    return this.registrationform.controls;
+  }
+
+  onSubmit(){
+    const data={
+      username: this.registration.username.value,
+      email: this.registration.email.value,
+      password: this.registration.password.value,
+
+    };
+    this.registerservice.register(data).subscribe(
+      (response) =>{
+        alert('Registration successful');
+        this.router.navigate(['login']);
+      },
+      (error)=>{
+        console.log(error)
+        alert('Registration not successful')
+      }
+    );
+  }
+
+
 
 }
--- blog/src/app/services/auth/register.service.ts
+++ blog/src/app/services/auth/register.service.ts
@@ -7,7 +7,20 @@ import { Injectable } from '@angular/core';
 })
 export class RegisterService {
   registerUrl ='http://127.0.0.1:8000/api/register'
+  userUrl = 'http://127.0.0.1:8000/api/user'
 
   constructor(private http:HttpClient) { }
+
+  register(data: { username: string; email: string; password: string }) {
+    return this.http.post(this.registerUrl, data);
+  }
+
+  getUser() {
+    return this.http.get(this.userUrl, { withCredentials: true });
+  }
+
+
+
+
 }
 
