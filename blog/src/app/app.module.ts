390e3324140cd03c6c7fd681caf7496116defeda
Joy Kirii <joykirii@gmail.com>
2021-06-09 18:09:20 +0300

    set up homepage design

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -1,37 +1,35 @@
-import { AuthModule } from './auth/auth.module';
 import { ModuleWithProviders } from '@angular/core';
-import { HomeModule } from './home/home.module';
-import { HeaderComponent } from './shared/layout/header.component';
-import { FooterComponent } from './shared/layout/footer.component';
+
 import { RouterModule } from '@angular/router';
-import { SharedModule } from './shared/shared.module';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AuthComponent } from './auth/auth.component';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+
 
 const rootRouting: ModuleWithProviders<any> = RouterModule.forRoot([], { useHash: true });
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    HeaderComponent,
+    NavbarComponent,
     FooterComponent,
-    AuthComponent
+    HomeComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule,
-    HomeModule,
+    
     rootRouting,
-    AuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
